7f55eeb37ced3b6a1ebe1a34c7fd0f966ae59d54
DDeenis <ddeenis46@gmail.com>
2024-03-10 14:03:00 +0000

    add details to product card

--- src/components/MainPage/CarouselProduct.tsx
+++ src/components/MainPage/CarouselProduct.tsx
@@ -8,6 +8,7 @@ import {
 } from "@/components/ui/carousel";
 import Image from "next/image";
 import placeholderImage from "../../../public/placeholder.svg";
+import { MessageCircle, StarIcon } from "lucide-react";
 
 export function CarouselProduct() {
   return (
@@ -38,10 +39,21 @@ const ProductCard = ({ title, price }: { title: string; price: number }) => {
       <div className="p-2">
         <Image src={placeholderImage} alt="placeholder" className="w-full" />
         <div className="mt-3 flex flex-col">
-          <span className="text-lg">{title}</span>
+          <span className="text-lg line-clamp-2">{title}</span>
+          <div className="py-1 flex gap-3 items-center">
+            <div className="flex items-center gap-1">
+              <StarIcon width={16} height={16} />
+              <span className="text-sm">4.7</span>
+            </div>
+            <div className="flex items-center gap-1">
+              <MessageCircle width={16} height={16} />
+              <span className="text-sm">228</span>
+            </div>
+          </div>
           <div>
             <span className="text-xl">${whole}</span>
             <sup>{fraction}</sup>
+            <sub className="ml-2 line-through text-gray-400">$39.99</sub>
           </div>
         </div>
       </div>
