e3b912e7c77cb34a619f38a79f8bd64e0d31f94e
DDeenis <ddeenis46@gmail.com>
2024-04-22 12:25:46 +0100

    fix styles bug

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,12 +1,5 @@
 import type { Metadata } from "next";
-import { Mulish } from "next/font/google";
 import "../globals.css";
-import Providers from "../providers";
-
-const mulish = Mulish({
-  subsets: ["latin"],
-  // weight: ["400", "500", "600", "700"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
