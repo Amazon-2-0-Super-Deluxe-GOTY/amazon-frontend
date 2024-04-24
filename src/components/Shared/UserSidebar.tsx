3ee9a06355a4ca161b720d9bdeb963ee691418de
Iryna <VacX_vb15@student.itstep.org>
2024-04-24 16:58:40 +0300

    add sign-in-up modal open

--- src/components/Shared/UserSidebar.tsx
+++ src/components/Shared/UserSidebar.tsx
@@ -23,6 +23,8 @@ import {
 import Link from "next/link";
 import { Button } from "../ui/button";
 import { useSearchParamsTools } from "@/lib/router";
+import { useEffect, useState } from "react";
+import { ModalSignInUpVariation } from "../SignInUpModal/ModalSignInUpVariation";
 
 export const UserSidebar = ({
   isOpen,
@@ -36,6 +38,13 @@ export const UserSidebar = ({
   categories: { icon: React.ReactNode; title: string; url: string }[];
 }) => {
   const searchParams = useSearchParamsTools();
+  
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(() => {
+    const defaultValue = searchParams.get("modal");
+    if (defaultValue) return true;
+
+    return false;
+  });
 
   const isLoggedIn = !!user;
 
@@ -47,6 +56,17 @@ export const UserSidebar = ({
     closeSidebar();
     searchParams.set("modal", "signup");
   };
+  const closeModal = () => {
+    searchParams.set("modal", undefined);
+  };
+  
+  useEffect(() => {
+    if (searchParams.get("modal")) setIsModalOpen(true);
+  }, [openLogInModal, openSignUpModal]);
+
+  useEffect(() => {
+    if (!searchParams.get("modal")) setIsModalOpen(false);
+  }, [closeModal]);
 
   const headerData = {
     avatarImage: isLoggedIn ? user.avatar : "",
@@ -58,60 +78,63 @@ export const UserSidebar = ({
   };
 
   return (
-    <Sidebar isOpen={isOpen} closeModal={closeSidebar}>
-      <SidebarHeader>
-        <SidebarAvatar
-          image={headerData.avatarImage}
-          fallback={headerData.avatarFallback}
-        />
-        <div>
-          <SidebarTitle>{headerData.title}</SidebarTitle>
-          <SidebarDescription>{headerData.description}</SidebarDescription>
-        </div>
-        {!isLoggedIn && (
-          <div className="flex flex-col gap-2 pt-2 lg:pt-4">
-            <Button onClick={openSignUpModal}>Sign up</Button>
-            <Button onClick={openLogInModal} variant={"secondary"}>
-              Log in
-            </Button>
+    <div>
+      <Sidebar isOpen={isOpen} closeModal={closeSidebar}>
+        <SidebarHeader>
+          <SidebarAvatar
+            image={headerData.avatarImage}
+            fallback={headerData.avatarFallback}
+          />
+          <div>
+            <SidebarTitle>{headerData.title}</SidebarTitle>
+            <SidebarDescription>{headerData.description}</SidebarDescription>
           </div>
-        )}
-      </SidebarHeader>
-      <Separator />
-      <Accordion type="single" collapsible>
-        <AccordionItem value="item-1" className="border-none">
-          <AccordionTrigger className="py-3 lg:data-[state=open]:pb-4">
-            <SidebarItem icon={<ShoppingBagIcon />} text="Product catalog" />
-          </AccordionTrigger>
-          <AccordionContent className="flex flex-col gap-3 lg:gap-4">
-            {categories.map((item) => (
-              <Link className="w-full" href={item.url} key={item.title}>
-                <SidebarItem icon={item.icon} text={item.title} />
-              </Link>
-            ))}
-            <Button variant={"outline"}>See all</Button>
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion>
-      <Separator />
-      <div className="flex flex-col gap-3 lg;gap-4">
-        {isLoggedIn && (
-          <Link href={"/profile/settings"}>
-            <SidebarItem icon={<SettingsIcon />} text="Settings" />
+          {!isLoggedIn && (
+            <div className="flex flex-col gap-2 pt-2 lg:pt-4">
+              <Button onClick={openSignUpModal}>Sign up</Button>
+              <Button onClick={openLogInModal} variant={"secondary"}>
+                Log in
+              </Button>
+            </div>
+          )}
+        </SidebarHeader>
+        <Separator />
+        <Accordion type="single" collapsible>
+          <AccordionItem value="item-1" className="border-none">
+            <AccordionTrigger className="py-3 lg:data-[state=open]:pb-4">
+              <SidebarItem icon={<ShoppingBagIcon />} text="Product catalog" />
+            </AccordionTrigger>
+            <AccordionContent className="flex flex-col gap-3 lg:gap-4">
+              {categories.map((item) => (
+                <Link className="w-full" href={item.url} key={item.title} onClick={closeSidebar}>
+                  <SidebarItem icon={item.icon} text={item.title} />
+                </Link>
+              ))}
+              <Button variant={"outline"}>See all</Button>
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+        <Separator />
+        <div className="flex flex-col gap-3 lg;gap-4">
+          {isLoggedIn && (
+            <Link href={"/profile/settings"}>
+              <SidebarItem icon={<SettingsIcon />} text="Settings" />
+            </Link>
+          )}
+          <Link href={"/help"}>
+            <SidebarItem icon={<HelpCircleIcon />} text="Help & FAQ" />
           </Link>
+        </div>
+        {isLoggedIn && (
+          <>
+            <Separator />
+            <button>
+              <SidebarItem icon={<LogOutIcon />} text="Log out" />
+            </button>
+          </>
         )}
-        <Link href={"/help"}>
-          <SidebarItem icon={<HelpCircleIcon />} text="Help & FAQ" />
-        </Link>
-      </div>
-      {isLoggedIn && (
-        <>
-          <Separator />
-          <button>
-            <SidebarItem icon={<LogOutIcon />} text="Log out" />
-          </button>
-        </>
-      )}
-    </Sidebar>
+      </Sidebar>
+      {!isLoggedIn && isModalOpen && <ModalSignInUpVariation onClose={closeModal} />}
+    </div>
   );
 };
