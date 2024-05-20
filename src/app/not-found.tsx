e26d8d49e702a16275f872f8ee1df266925a9b82
DDeenis <ddeenis46@gmail.com>
2024-05-20 08:59:09 +0100

    fix tailwind

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,19 +1,22 @@
-import "./globals.css";
-import Image from 'next/image';
-import Link from 'next/link';
+import Image from "next/image";
+import Link from "next/link";
 import placeholder from "@/../public/Icons/placeholder.svg";
-import { Button } from '@/components/ui/button';
- 
+import { Button } from "@/components/ui/button";
+
 export default function NotFound() {
   return (
-    <div className='flex justify-center items-center'>
-      <div className='flex justify-center items-center'>
-        <Image src={placeholder} alt="Placeholder" className=" max-w-lg max-h-96" /> 
+    <div className="flex justify-center items-center">
+      <div className="flex justify-center items-center">
+        <Image
+          src={placeholder}
+          alt="Placeholder"
+          className=" max-w-lg max-h-96"
+        />
         <span>This page has gone fishing...</span>
         <Button variant={"default"}>
           <Link href="/">Return to main page</Link>
         </Button>
       </div>
     </div>
-  )
+  );
 }
