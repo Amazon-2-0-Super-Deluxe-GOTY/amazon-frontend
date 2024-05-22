a649944eb90ce6dda48f9139aa5b17b8b5eebc3d
Iryna <VacX_vb15@student.itstep.org>
2024-05-22 12:47:19 +0300

    fix styles and add mobile version

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,22 +1,27 @@
 import Image from "next/image";
-import Link from "next/link";
 import placeholder from "@/../public/Icons/placeholder.svg";
 import { Button } from "@/components/ui/button";
 
 export default function NotFound() {
   return (
-    <div className="flex justify-center items-center">
-      <div className="flex justify-center items-center">
-        <Image
-          src={placeholder}
-          alt="Placeholder"
-          className=" max-w-lg max-h-96"
-        />
-        <span>This page has gone fishing...</span>
-        <Button variant={"default"}>
-          <Link href="/">Return to main page</Link>
+  <div className="flex justify-center items-center h-screen">
+    <div className="flex flex-col justify-center items-center lg:gap-10 md:gap-7 gap-4 sm:px-8">
+      <div className="flex flex-col justify-center items-center gap-2">
+        <div className="flex max-w-[800px] max-h-[550px] min-h-[430px]">
+          <Image
+            src={placeholder}
+            alt="Placeholder"
+            className="object-cover" 
+          />
+        </div>
+        <span className="lg:text-2xl md:text-xl sm:text-base text-sm mx-3">This page has gone fishing...</span>
+      </div>
+      <div className="flex w-full justify-center">
+        <Button variant={"default"} className="sm:max-w-[400px] max-w-[260px] max-h-14 w-full h-full md:py-4 sm:py-3 py-2 mx-3">
+          <a href="/" className="lg:text-xl text-base">Return to main page</a>
         </Button>
       </div>
     </div>
+  </div>
   );
 }
